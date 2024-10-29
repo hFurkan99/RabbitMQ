eb3779f7561e477ab3ce91592c60ac4bc8595d2d
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2024-10-29 15:04:32 +0300

    Hello World with RabbitMQ

---
+++ RabbitMQBasics/RabbitMQBasics/RabbitMQBasics.Subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received: {message}");
+};
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
