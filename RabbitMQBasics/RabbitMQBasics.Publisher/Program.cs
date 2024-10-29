1cbb03652c8c32ceab3cb074eac493a536a19a1b
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2024-10-29 15:27:04 +0300

    Multiplte consumer

---
+++ RabbitMQBasics/RabbitMQBasics.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "work-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+foreach (var item in Enumerable.Range(1, 100))
+{
+    string message = $"Message {item}";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty, routingKey: "work-queue", basicProperties: null, body: body);
+
+    Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
--- RabbitMQBasics/RabbitMQBasics.Subscriber/Program.cs
+++ RabbitMQBasics/RabbitMQBasics.Subscriber/Program.cs
@@ -6,10 +6,11 @@ var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-//channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+//channel.QueueDeclare(queue: "work-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
 Console.WriteLine(" [*] Waiting for messages.");
 
+channel.BasicQos(0, 1, false);
 var consumer = new EventingBasicConsumer(channel);
 
 consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
@@ -17,9 +18,11 @@ consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
     var body = e.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
     Console.WriteLine($" [x] Received: {message}");
+    //Bu işlemden sonra kuyruktaki veriyi sil
+    channel.BasicAck(e.DeliveryTag, false);
 };
 
-channel.BasicConsume("hello-queue", true, consumer);
+channel.BasicConsume("work-queue", false, consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
