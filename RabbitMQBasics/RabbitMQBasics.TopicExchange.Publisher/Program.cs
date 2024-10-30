983451ab5444ed6bcc8d160f5c5165a2b4cd66e9
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2024-10-31 01:07:24 +0300

    Topic Exchange

---
+++ RabbitMQBasics/RabbitMQBasics.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare("logs-topic", ExchangeType.Topic, true);
+
+
+Random random = new();
+
+foreach (var item in Enumerable.Range(1, 100))
+{
+
+    LogNames logType1 = (LogNames)random.Next(1, 5);
+    LogNames logType2 = (LogNames)random.Next(1, 5);
+    LogNames logType3 = (LogNames)random.Next(1, 5);
+
+    var routeKey = $"{logType1}.{logType2}.{logType3}";
+    string message = $"log type: {logType1}-{logType2}-{logType3}";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+
+    channel.BasicPublish(exchange: "logs-topic", routeKey, basicProperties: null, body: body);
+
+    Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ RabbitMQBasics/RabbitMQBasics.TopicExchange.Subscriber/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var queueName = channel.QueueDeclare().QueueName;
+var routeKey = "*.Error.*";
+channel.BasicQos(0, 1, false);
+
+channel.QueueBind(queueName, "logs-topic", routeKey);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received: {message}");
+    Thread.Sleep(200);
+
+
+    //Bu işlemden sonra kuyruktaki veriyi sil
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
