a9c2e04788eb9e7b56b24d9fc1f535b8f74ef9b1
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2024-10-31 01:26:13 +0300

    Header Exchange

---
+++ RabbitMQBasics/RabbitMQBasics.HeaderExchange.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("header-exchange", ExchangeType.Headers, true, false);
+
+Dictionary<string, object> headers = new()
+{
+    { "format", "pdf" },
+    { "shape", "a4" }
+};
+
+var properties = channel.CreateBasicProperties();
+properties.Headers = headers;
+
+foreach (var item in Enumerable.Range(1, 100))
+{
+    string message = $"Message {item}";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "header-exchange", routingKey: string.Empty, basicProperties: properties, body: body);
+
+    Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ RabbitMQBasics/RabbitMQBasics.HeaderExchange.Subscriber/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+var randomQueueName = channel.QueueDeclare().QueueName;
+
+Dictionary<string, object> headers = new()
+{
+    { "f3ormat", "pdf" },
+    { "shape", "a4" },
+    { "x-match", "any" }
+};
+
+channel.QueueBind(randomQueueName, "header-exchange", string.Empty, headers);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received: {message}");
+    //Bu işlemden sonra kuyruktaki veriyi sil
+    Thread.Sleep(250);
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+channel.BasicConsume(randomQueueName, false, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
