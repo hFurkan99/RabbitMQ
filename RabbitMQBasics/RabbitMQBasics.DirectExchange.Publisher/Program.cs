032a54ed21ca20742e4099d6ada5d27bea361255
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2024-10-31 00:48:45 +0300

    Direct Exchange

---
+++ RabbitMQBasics/RabbitMQBasics.DirectExchange.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare("logs-direct", ExchangeType.Direct, true);
+
+foreach (var logType in Enum.GetNames(typeof(LogNames)).ToList())
+{
+    var routeKey = $"route-{logType}";
+    var queueName = $"direct-queue-{logType}";
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, "logs-direct", routeKey);
+}
+foreach (var item in Enumerable.Range(1, 100))
+{
+    LogNames logType = (LogNames)new Random().Next(1, 5);
+    string message = $"log {item} --- log-type: {logType}";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    var routeKey = $"route-{logType}";
+
+    channel.BasicPublish(exchange: "logs-direct", routeKey , basicProperties: null, body: body);
+
+    Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ RabbitMQBasics/RabbitMQBasics.DirectExchange.Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var queueName = "direct-queue-Critical";
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received: {message}");
+    Thread.Sleep(200);
+
+    File.AppendAllText("log-critical.txt", message + "\n");
+
+    //Bu işlemden sonra kuyruktaki veriyi sil
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
